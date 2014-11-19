0106b51b69857a67019b4afe3a2ff1a82580b2d4
DavidHdw <tossboydavid@sina.com>
2014-11-19 14:32:07 +0800

    config indexes

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,6 +9,15 @@ class Article < ActiveRecord::Base
   include Elasticsearch::Model::Callbacks
 
   scope :search_text, ->(query){ search(query).records if query }
+  mapping do
+    indexes :id, type: 'integer'
+    indexes :author_id, type: 'integer'
+    indexes :author_name
+    indexes :name, boost: 10       # rank score weight
+    indexes :content, analyzer:'snowball'
+    indexes :published_at, type: 'date'
+    indexes :comments_count, type: 'integer'
+  end
 
 end
 
